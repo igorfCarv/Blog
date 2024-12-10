2be1af8998c718da9c7e50a687c93974af76c95b
Igor Carvalho <igorcarvalho@aridesa.com.br>
2024-12-10 12:45:48 -0300

    Updating Guest page

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -2,9 +2,9 @@
 @section('content')
     <div class="w-full text-center py-32">
         <h1 class="text-2xl md:text-3xl font-bold text-center lg:text-5xl text-gray-700">
-            Welcome to <span class="text-yellow-500">&lt;YELO&gt;</span> <span class="text-gray-900"> News</span>
+            Bem-vindo ao <span class="text-yellow-500">&lt;BLOG&gt;</span> <span class="text-gray-900"> </span>
         </h1>
-        <p class="text-gray-500 text-lg mt-1">Best Blog in the universe</p>
+        <p class="text-gray-500 text-lg mt-1">Desafio Laravel Dev Web</p>
         <a class="px-3 py-2 text-lg text-white bg-gray-800 rounded mt-5 inline-block"
            href="http://127.0.0.1:8000/blog">Start
             Reading</a>
