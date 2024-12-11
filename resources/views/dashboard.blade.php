79f13342ed5f2cc8d8863ef4a823e28da300fef9
Igor Carvalho <igorcarvalho@aridesa.com.br>
2024-12-11 17:25:26 -0300

    Creating views dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,21 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+    <section class="flex flex-col">
+    <h2> Bem-vindo Igor! </h2>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
-            </div>
+    <div class="flex gap-2">
+        <div class="transition-all duration-300 bg-white p-4 rounded-lg shadow-md border-l-4 border-blue-600">
+            <h3 class="text-2xl font-semibold mb-2 text-blue-600">Quantidade de Postagens:</h3>
+            <p class="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquam justo nec justo lacinia, vel ullamcorper nibh tincidunt.</p>
+            <a href="{{route('posts.create')}}">Criar novo post</a>
+        </div>
+        <div class="transition-all duration-300 bg-white p-4 rounded-lg shadow-md border-l-4 border-green-600">
+            <h3 class="text-2xl font-semibold mb-2 text-blue-600">Quantidade de Categorias:</h3>
+            <p class="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquam justo nec justo lacinia, vel ullamcorper nibh tincidunt.</p>
+            <a href="{{route('posts.create')}}">Criar nova categoria</a>
         </div>
     </div>
+
+    <h4>Veja suas últimas postagens</h4>
+    </section>
+
 </x-app-layout>
